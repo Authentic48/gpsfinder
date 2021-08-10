ff50f3dcb186c67f966a980487b5d0aa38547c11
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-08-10 16:46:00 +0700

    DB connection done

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const { sequelize }= require('./config/db')
+
 const app = express()
 
-const PORT = process.env.PORT ||  5000 
+dotenv.config()
 
-app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} on port ${PORT}`))
+//DB test 
+sequelize.sync()
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log('Error: ' + err))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} on port ${process.env.PORT}`))
